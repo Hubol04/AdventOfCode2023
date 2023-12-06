9d874ce3bec9c870459a82f4b1b16eb299097f3e
Hubol04 <90552048+Hubol04@users.noreply.github.com>
2023-12-06 15:39:54 +0100

    solved day5 task 2 via brute force

--- day5/day5.py
+++ day5/day5.py
@@ -78,5 +78,63 @@ def threadwork(seed, vals, input):
                 break
     vals.append(val)
 
+def task2brute():
+    input = getformatedinput()
+    min = 46200000   #already in previous run tested
+    while (0==0):
+        if(min%100000 == 0):
+            print(min)
+        val = min
+        for i in range(len(input)-1, 0, -1):
+            for conversion in input[i]:
+                tmp = int(val) - int(conversion[0])
+                if (tmp < int(conversion[2])) and (tmp >= 0):
+                    val = int(conversion[1]) + tmp
+                    break
+        for k in range(0, len(input[0]), 2):
+            if (val >= int(input[0][k])) and (val < int(input[0][k]) + int(input[0][k+1])):
+                print("Min is: " + str(min))
+                return
+        min += 1
+
+def brutethreads(min, stop, found, input):
+    while (min < stop):
+        if(min%100000 == 0):
+                print(min)
+        val = min
+        for i in range(len(input)-1, 0, -1):
+            for conversion in input[i]:
+                tmp = int(val) - int(conversion[0])
+                if (tmp < int(conversion[2])) and (tmp >= 0):
+                    val = int(conversion[1]) + tmp
+        for k in range(0, len(input[0]), 2):
+            if val in range(int(input[0][k]), int(input[0][k]) + int(input[0][k+1])):
+                print("Min is: " + str(min))
+                found = 1
+                return
+        min += 1
+    print("Values from: " + str(min) + " to: " + str(stop) + " no seed found")
+
+def task2brutethreads():
+    threads = []
+    found = 0
+    input = getformatedinput()
+    min = 0
+    while (0==0):
+        for i in range(4):
+            x = threading.Thread(target = brutethreads, args = (min+i*1000000, min+(i+1)*1000000, found, input))
+            threads.append(x)
+            x.start()
+            print("Thread " + str(i) + " created.")
+        for thread in threads:
+            print("Wait for Thread " + str(i) + ".")
+            thread.join
+            threads = []
+        if found == 1:
+            print("Stop because found Min")
+            return
+        else:
+            min += 4000000
+
 task1()
-task2thread()
+task2brute()    #0 to 46.200.000 already tested
